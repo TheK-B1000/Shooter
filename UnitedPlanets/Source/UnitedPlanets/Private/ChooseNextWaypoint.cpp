4428fee54548e45f18123835c05d3559d363ad7c
Kevin-Brandon Joseph Corbett <k-b0514@hotmail.com>
2020-01-08 22:06:12 -0500

    05 - Add nav mesh, AI Controller and Patrol Route Component

---
+++ UnitedPlanets/Source/UnitedPlanets/Private/ChooseNextWaypoint.cpp
@@ -0,0 +1,22 @@
+// K-B Enterprises
+
+
+#include "ChooseNextWaypoint.h"
+#include "AIController.h"
+#include "PatrolRoute.h"
+#include "BehaviorTree/BlackboardComponent.h"
+
+EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	// Get Patrol Points 
+	auto AIController = OwnerComp.GetAIOwner();
+	auto ControlledPawn = AIController->GetPawn();
+	auto PatrolRoute = ControlledPawn->FindComponentByClass<UPatrolRoute>();
+	auto PatrolPoints = PatrolRoute->PatrolPointsCPP;
+
+	// Choose Next Waypoint
+
+	// Cycle Index
+
+	return EBTNodeResult::Succeeded;
+}
